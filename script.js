91a1fde7331b4b702407023d38cf1d5015db5a3e
nori0507 <chikyu.rei@ic.edu>
2026-09-09 23:54:18 -0500

    initial and final commit

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function renderTasks() {
                     <div class="task-content">
                         <div class="task-title">${task.title}</div>
                         <div class="task-description">${task.description}</div>
-                        <div class="task-date">Due: ${task.dueDate}</div>
+                        <div id="first-task-due">Due: ${task.dueDate}</div>
                         ${task.assignedTo ? `<div class="task-date">Assigned to: ${task.assignedTo}</div>` : ''}
                     </div>
                     <div class="task-actions">
@@ -42,9 +42,34 @@ function renderTasks() {
     });
 }
 
+// function for the over due date, changing color when the color is over due date
+function overDueTasks(dateId,dueDate){
+    const currentDate = new Date();
+    const taskDueDate = new Date(dueDate);
+
+    const targetElement = document.getElementById(dateId);
+
+    if(currentDate > taskDueDate){
+        targetElement.style.color="red";
+    }
+}
+
+// function to show a small window to notify the success of the additon of new tasks
+function taskForm() {
+    const addBtn = document.getElementById('add-btn');
+    
+    if (addBtn) {
+        addBtn.addEventListener('click', () => {
+            alert('A Task Added succesfully!');
+        });
+    }
+}
+
 // Initialize the app when page loads
 document.addEventListener('DOMContentLoaded', function() {
     renderTasks();
-    
-    // TODO
+    overDueTasks("first-task-due","2025-10-15");
+    taskForm();
 });
+
+
